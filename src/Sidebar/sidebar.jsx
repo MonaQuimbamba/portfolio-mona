b7bf71bc58a256ff7255d25689dee1010b4ab7e5
monaquimbamba <claudio.antonio@unilim.fr>
2022-05-27 22:46:57 +0200

    add

--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -61,7 +61,7 @@ const Line = styled.div`
 const Sidebar = ({ handleToast }) => {
 	// eslint-disable-next-line
 	const handleClipboardClick = () => {
-		navigator.clipboard.writeText('claudio.antonio.cantonio@outlook.com');
+		navigator.clipboard.writeText('dannychukz15@gmail.com');
 		handleToast(`Email copy to clipboard! 🥳`);
 		return;
 	};
@@ -70,20 +70,32 @@ const Sidebar = ({ handleToast }) => {
 		<>
 			<LeftSideContainer>
 				<SideIcon whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.85 }}>
-					<a href='https://github.com/MonaQuimbamba' target='_blank' rel='noopener noreferrer'>
+					<a
+						href='https://drive.google.com/file/d/1iUdfXirmVZJ58RKBDNnUGVGnmKsH7cKB/view?usp=sharing'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<FilePdf />
+					</a>
+				</SideIcon>
+				<SideIcon whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.85 }}>
+					<a href='https://github.com/CrazyChickenDev' target='_blank' rel='noopener noreferrer'>
 						<Github />
 					</a>
 				</SideIcon>
-
 				<SideIcon whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.85 }}>
-					<a href='https://www.linkedin.com/in/claudio-antonio-0038b8194/' target='_blank' rel='noopener noreferrer'>
+					<a href='https://twitter.com/CrazyChickenDev' target='_blank' rel='noopener noreferrer'>
+						<Twitter />
+					</a>
+				</SideIcon>
+				<SideIcon whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.85 }}>
+					<a href='https://www.linkedin.com/in/nwaobi-daniel-8b4527187/' target='_blank' rel='noopener noreferrer'>
 						<LinkedinSquare />
 					</a>
 				</SideIcon>
 				<Line />
 			</LeftSideContainer>
 			<RightSideContainer>
-				<SideText>claudio.antonio.cantonio@outlook.com</SideText>
+				<SideText>dannychukz15@gmail.com</SideText>
 				<Line />
 			</RightSideContainer>
 		</>
